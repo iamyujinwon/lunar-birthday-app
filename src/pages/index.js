b728fe81aebc23a3d7fba17b21ba90e901e6efe4
Yujin Won <iamyujinwon@gmail.com>
2022-04-13 22:30:34 -0400

    make it possible to select year, month, and day based on user's birthday

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,45 @@
 import React, {useState} from 'react';
 import './index.css';
+import ReactDOM from 'react-dom';
+
+
+const currentYear = new Date().getUTCFullYear();
+
+const years = [{value: "YEAR", text: "YEAR"}];
+for (let i = currentYear; i > 1899; i--) {
+    years.push({"value": i, "text": i});
+}
+
+const months = [
+    {value: "MONTH", text: "MONTH"},{value: "JANUARY", text: "JANUARY"},{value: "FEBRUARY", text: "FEBRUARY"},
+    {value: "MARCH", text: "MARCH"},{value: "APRIL", text: "APRIL"},
+    {value: "MAY", text: "MAY"},{value: "JUNE", text:"JUNE"},
+    {value: "JULY", text:"JULY"},{value: "AUGUST", text:"AUGUST"},
+    {value: "SEPTEMBER", text:"SEPTEMBER"},{value: "OCTOBER", text:"OCTOBER"},
+    {value: "NOVEMBER", text:"NOVEMBER"},{value: "DECEMBER", text:"DECEMBER"}
+];
+
+const days = [{value: "DAY", text: "DAY"}];
+for (let i = 1; i < 32; i++) {
+    days.push({"value": i, "text": i});
+}
 
 const Home = () =>  {
+
+    const [year, setYear] = React.useState('YEAR');
+    const handleChange = e => setYear(e.target.value);
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        const data = {
+            year: year
+        };
+
+        const json = JSON.stringify(data);
+        console.clear();
+        console.log(json);
+    }
+
     return (
         <> 
             <div id="container">
@@ -10,36 +48,41 @@ const Home = () =>  {
                         When is <br />
                         my <span id="lunar">lunar</span> <br/>
                         birthday <br />
-                        in 2022?
+                        in {currentYear}?
                     </div>
                 </div>
                 <div id ="date-section">
-                    <form id="date-containter">
+                    <form id="date-containter" onSubmit={handleSubmit}>
                         <div>Enter my birthday 🎂</div> 
                         <div id="date-selector">
                             <span>  
-                                <select name="year" id="year">
-                                    <option>YEAR</option>
-                                    <option value="years">1999</option>
+                                {/* <Select value={year} option={years} onChange={handleChange} /> */}
+                            
+                                <select value={year} onChange={handleChange}>
+                                    {years.map(year => {
+                                        return (<option key={year.value} value={year.value}>{year.text}</option>);
+                                    })}
                                 </select>
                             </span>   
                             <span>
-                                <select name="month" id="month">
-                                    <option>MONTH</option>
-                                    <option value="months">MAY</option>
+                                <select>
+                                    {months.map(month => {
+                                        return (<option key={month.value} value={month.value}>{month.text}</option>);
+                                    })}
                                 </select>
                             </span> 
                             <span>
-                                <select name="day" id="day">
-                                    <option>DAY</option>
-                                    <option value="days">17</option>
+                                <select>
+                                    {days.map(day => {
+                                        return (<option key={day.value} value={day.value}>{day.text}</option>);
+                                    })}
                                 </select>
                             </span> 
                         </div>
+                        <button type="submit">
+                            Find my lunar birthday
+                        </button>
                     </form>
-                    <button type="button">
-                        Find my lunar birthday
-                    </button>
                 </div>
             </div>
         </>
