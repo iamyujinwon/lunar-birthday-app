8502ff60eaf32224f89123dc2f90eb005a0751f4
Yujin Won <iamyujinwon@gmail.com>
2022-04-18 22:25:06 -0400

    import lunar-date-calculator

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,17 @@
 import React, {useState} from 'react';
 import './index.css';
 import ReactDOM from 'react-dom';
+import lunaDateCalculator from 'lunar-date-calculator';
 
 const currentYear = new Date().getUTCFullYear();
 
-const years = [{value: "YEAR", text: "YEAR"}];
+const years = [];
 for (let i = currentYear; i > 1899; i--) {
     years.push({"value": i, "text": i});
 }
 
 const months = [
-    {value: "MONTH", text: "MONTH"},{value: 1, text: "JANUARY"},{value: 2, text: "FEBRUARY"},
+    {value: 1, text: "JANUARY"},{value: 2, text: "FEBRUARY"},
     {value: 3, text: "MARCH"},{value: 4, text: "APRIL"},
     {value: 5, text: "MAY"},{value: 6, text:"JUNE"},
     {value: 7, text:"JULY"},{value: 8, text:"AUGUST"},
@@ -18,15 +19,17 @@ const months = [
     {value: 11, text:"NOVEMBER"},{value: 12, text:"DECEMBER"}
 ];
 
-const days = [{value: "DAY", text: "DAY"}];
+const days = [];
 for (let i = 1; i < 32; i++) {
     days.push({"value": i, "text": i});
 }
 
 const Home = () =>  {
-    const [year, setYear] = React.useState('YEAR');
-    const [month, setMonth] = React.useState('MONTH');
-    const [day, setDay] = React.useState('DAY');
+    const [year, setYear] = useState(0);
+    const [month, setMonth] = useState(0);
+    const [day, setDay] = useState(0);
+
+    const [lunarBirthday, setLunarBirthday] = useState();
 
     const birthday = {year, month, day};
 
@@ -48,6 +51,17 @@ const Home = () =>  {
         console.log(json);
     }
 
+    function submitDisabled() {
+        console.log(birthday.year)
+        console.log(birthday.month)
+        console.log(birthday.day)
+        return birthday.year == 0 || birthday.month == 0 || birthday.day == 0
+    }
+
+    function resultRender() {
+        return false;
+    }
+
     return (
         <> 
             <div id="container">
@@ -59,6 +73,59 @@ const Home = () =>  {
                         in {currentYear}?
                     </div>
                 </div>
+
+
+                {resultRender() == true? 
+                    <div id="result-container">
+                        <div id="result-section">
+                            <div>My solar birthday is <span id="solar-birthday">{birthday.year}/{birthday.month}/{birthday.day}</span></div>
+                            <div>This year, my lunar birthday is</div>
+                            <div className="date-result">2022/06/18</div>
+                            <div>Next year, my lunar birthday is</div>
+                            <div className="date-result">2023/06/05</div>
+                        </div>
+                        <button id="google-calendar">
+                            Add the date in Google Calendar
+                        </button>
+                    </div>
+                    : 
+                    <div id ="date-section">
+                    <form id="date-containter" onSubmit={handleSubmit}>
+                        <div>Enter the Solar birthday 🎂</div> 
+                        <div id="date-selector">
+                            <span>  
+                                <select value={year} onChange={e=>(setYear(e.target.value))}>
+                                    <option value={0} default>YEAR</option>
+                                    {years.map(year => {
+                                        return (<option key={year.value} value={year.value}>{year.text}</option>);
+                                    })}
+                                </select>
+                            </span>   
+                            <span>
+                                <select onChange={e=>(setMonth(e.target.value))}>
+                                    <option value={0} default>MONTH</option>
+                                    {months.map(month => {
+                                        return (<option key={month.value} value={month.value}>{month.text}</option>);
+                                    })}
+                                </select>
+                            </span> 
+                            <span>
+                                <select onChange={e=>(setDay(e.target.value))}>
+                                    <option value={0} default>DAY</option>
+                                    {days.map(day => {
+                                        return (<option key={day.value} value={day.value}>{day.text}</option>);
+                                    })}
+                                </select>
+                            </span> 
+                        </div>
+                        <button type="submit" disabled={submitDisabled()}>
+                            Find my lunar birthday
+                        </button>
+                    </form>
+                </div> 
+                
+                }
+
                 {/* <div id ="date-section">
                     <form id="date-containter" onSubmit={handleSubmit}>
                         <div>Enter the Solar birthday 🎂</div> 
@@ -91,7 +158,7 @@ const Home = () =>  {
                     </form>
                 </div> */}
 
-                <div id="result-container">
+                {/* <div id="result-container">
                     <div id="result-section">
                         <div>My lunar birthday is <span id="solar-birthday">{birthday.year}/{birthday.month}/{birthday.day}</span></div>
                         <div>This year, my lunar birthday is</div>
@@ -102,7 +169,7 @@ const Home = () =>  {
                     <button id="google-calendar">
                         Add the date in Google Calendar
                     </button>
-                </div>
+                </div> */}
             </div>
         </>
     );
